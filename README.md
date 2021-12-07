e3fe48c07c08d536f8537ae616ab7379dadfe08f
karlos <karlos>
2021-12-07 01:03:21 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# todo-list
+
+Todo list web application written in Javascript, HTML, CSS.
---
+++ app.js
@@ -0,0 +1,112 @@
+let todoItems = [];
+
+window.localStorage.setItem("todoItemsRef", JSON.stringify(todoItems));
+
+function renderTodo(todo) {
+  const list = document.querySelector(".js-todo-list");
+  // select the current todo item in the DOM
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
+  // add this if block
+  if (todo.deleted) {
+    // remove the item from the DOM
+    item.remove();
+
+    if (todoItems.length === 0) list.innerHTML = "";
+    return;
+  }
+
+  const isChecked = todo.checked ? "done" : "";
+  const node = document.createElement("li");
+  node.setAttribute("class", `todo-item ${isChecked}`);
+  node.setAttribute("data-key", todo.id);
+  node.innerHTML = `
+      <input id="${todo.id}" type="checkbox"/>
+      <label for="${todo.id}" class="tick js-tick"></label>
+      <span>${todo.text}</span>
+      <button class="delete-todo js-delete-todo">
+      <svg><use href="#delete-icon"></use></svg>
+      </button>
+    `;
+
+  // If the item already exists in the DOM
+  if (item) {
+    // replace it
+    list.replaceChild(node, item);
+  } else {
+    // otherwise append it to the end of the list
+    list.append(node);
+  }
+}
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+function toggleDone(key) {
+  // findIndex is an array method that returns the position of an element
+  // in the array.
+  const index = todoItems.findIndex((item) => item.id === Number(key));
+  // Locate the todo item in the todoItems array and set its checked
+  // property to the opposite. That means, `true` will become `false` and vice
+  // versa.
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+function deleteTodo(key) {
+  const index = todoItems.findIndex((item) => item.id === Number(key));
+
+  const todo = {
+    deleted: true,
+    ...todoItems[index],
+  };
+
+  todoItems = todoItems.filter((item) => item.id !== Number(key));
+  renderTodo(todo);
+}
+
+const form = document.querySelector(".js-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = document.querySelector(".js-todo-input");
+
+  const text = input.value.trim();
+  if (text != "") {
+    addTodo(text);
+    input.value = "";
+    input.focus();
+  }
+});
+
+// Select the entire list
+const list = document.querySelector(".js-todo-list");
+// Add a click event listener to the list and its children
+list.addEventListener("click", (event) => {
+  if (event.target.classList.contains("js-tick")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+
+  if (event.target.classList.contains("js-delete-todo")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ref = localStorage.getItem("todoItemsRef");
+  if (ref) {
+    todoItems = JSON.parse(ref);
+    todoItems.forEach((t) => {
+      renderTodo(t);
+    });
+  }
+});
